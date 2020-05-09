f28cb5aa44ffdd9faa8abe3df23905484d509d84
xBDL <xbdl@outlook.com>
2020-05-08 17:12:30 -0700

    created Show model

--- vagrant/fyyur/app.py
+++ vagrant/fyyur/app.py
@@ -57,7 +57,12 @@ class Artist(db.Model):
     seeking_description = db.Column(db.String(500))
     image_link = db.Column(db.String(500))
     
-# TODO Implement Show models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+  __tablename__ = 'Show'
+  id = db.Column(db.Integer, primary_key=True)
+  venue_id = db.Column(db.Integer, db.ForeignKey(Venue.id), nullable=False)
+  artist_id = db.Column(db.Integer, db.ForeignKey(Artist.id), nullable=False)
+  start_time = db.Column(db.String(30))
 
 #----------------------------------------------------------------------------#
 # Filters.
