58238c995f25dd472a40d0f63a4697282fbfefab
xBDL <xbdl@outlook.com>
2020-05-08 17:05:45 -0700

    adjusted Venue, completed Artist models

--- vagrant/fyyur/app.py
+++ vagrant/fyyur/app.py
@@ -31,8 +31,8 @@ migrate = Migrate(app, db)
 class Venue(db.Model):
     __tablename__ = 'Venue'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    genres = db.Column(db.ARRAY(db.String))
+    name = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String(30)))
     address = db.Column(db.String(120))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
@@ -40,24 +40,24 @@ class Venue(db.Model):
     website = db.Column(db.String(120)) 
     facebook_link = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean, default=False)
-    seeking_description = db.Column(db.String)
+    seeking_description = db.Column(db.String(500))
     image_link = db.Column(db.String(500))
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
-
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String(30)))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    website = db.Column(db.String(120))
     facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+    seeking_venue = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(500))
+    image_link = db.Column(db.String(500))
+    
+# TODO Implement Show models, and complete all model relationships and properties, as a database migration.
 
 #----------------------------------------------------------------------------#
 # Filters.
