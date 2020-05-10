6042018d18012ab3cad6fa2ee2cea27f2067717d
xBDL <xbdl@outlook.com>
2020-05-10 00:24:01 -0700

    connected shows() to db

--- vagrant/fyyur/app.py
+++ vagrant/fyyur/app.py
@@ -96,8 +96,6 @@ def venues():
   #       num_shows should be aggregated based on number of upcoming shows per venue.
 
   areas = {}
-
-  #venues = db.session.query(Venue.id, Venue.name, Venue.city, Venue.state).all()
   venues = Venue.query.with_entities(Venue.id, 
                                      Venue.name, 
                                      Venue.city, 
@@ -122,30 +120,7 @@ def venues():
                                   'name': name, 
                                   'num_upcoming_shows': num})
 
-  data=[{
-    "city": "San Francisco",
-    "state": "CA",
-    "venues": [{
-                "id": 1,
-                "name": "The Musical Hop",
-                "num_upcoming_shows": 0,
-              }, {
-                "id": 3,
-                "name": "Park Square Live Music & Coffee",
-                "num_upcoming_shows": 1,
-              }]
-  }, {
-    "city": "New York",
-    "state": "NY",
-    "venues": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }]
-
   areas = list(areas.values())
-  #data.extend(areas)
 
   return render_template('pages/venues.html', areas=areas);
 
@@ -416,44 +391,26 @@ def create_artist_submission():
 @app.route('/shows')
 def shows():
   # displays list of shows at /shows
-  # TODO: replace with real venues data.
-  #       num_shows should be aggregated based on number of upcoming shows per venue.
-  data=[{
-    "venue_id": 1,
-    "venue_name": "The Musical Hop",
-    "artist_id": 4,
-    "artist_name": "Guns N Petals",
-    "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-    "start_time": "2019-05-21T21:30:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 5,
-    "artist_name": "Matt Quevedo",
-    "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-    "start_time": "2019-06-15T23:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-01T20:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-08T20:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-15T20:00:00.000Z"
-  }]
+
+  data = [] 
+
+  shows = db.session.query(\
+            Show,\
+            Venue.name.label('venue_name'),\
+            Artist.name.label('artist_name'),\
+            Artist.image_link.label('artist_image_link'))\
+            .select_from(Show).\
+            join(Venue, Show.venue_id==Venue.id).\
+            join(Artist, Show.artist_id==Artist.id)
+
+  for show, venue_name, artist_name, artist_image_link in shows:
+    data.append({'venue_id': show.venue_id,
+                 'venue_name': venue_name,
+                 'artist_id': show.artist_id,
+                 'artist_name': artist_name,
+                 'artist_image_link': artist_image_link,
+                 'start_time': show.start_time})
+
   return render_template('pages/shows.html', shows=data)
 
 @app.route('/shows/create')
